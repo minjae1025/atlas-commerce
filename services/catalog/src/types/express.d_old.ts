cdd87b63ddda6213b9b446cdc7b7dcf1da5cbcc4
minjae1025 <kjt081025@gmail.com>
2026-06-26 12:13:29 +0900

    지원자 김민재 - supplement.md 보완점 작성

--- services/catalog/src/types/express.d_old.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  namespace Express {
-    interface Request {
-      id?: string;
-    }
-  }
-}
-
-export {};
--- services/gateway/src/utils/asyncHandler_old.ts
+++
@@ -1,7 +0,0 @@
-import type { NextFunction, Request, RequestHandler, Response } from 'express';
-
-export const asyncHandler =
-  (handler: (req: Request, res: Response, next: NextFunction) => Promise<void>): RequestHandler =>
-  (req, res, next) => {
-    void handler(req, res, next).catch(next);
-  };
